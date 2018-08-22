15627bf62031c872d7d73977c40226a3c20f49e3
Romonov <Romonov2003@outlook.com>
2018-08-23 07:32:46 +0800

    增加获取授权码API，可和Magicpush、SimpleMessenger对接。

---
+++ RUL/AccesssCode.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RUL
+{
+    public class AccesssCode
+    {
+        /// <summary>
+        /// 获取授权码
+        /// </summary>
+        /// <param name="Password">密码</param>
+        /// <returns>授权码</returns>
+        public static string GetAccessCode(string Password)
+        {
+            return MD5(MD5(Password) + TimeStamp());
+        }
+
+        private static string MD5(string strText)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] result = md5.ComputeHash(Encoding.Default.GetBytes(strText));
+            return Encoding.Default.GetString(result);
+        }
+
+        private static string TimeStamp()
+        {
+            string y = DateTime.Now.Year.ToString();
+            string M = DateTime.Now.Month.ToString();
+            string d = DateTime.Now.Day.ToString();
+            string H = DateTime.Now.Hour.ToString();
+            string m = DateTime.Now.Minute.ToString();
+
+            switch (M)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    M = "0" + M;
+                    break;
+                default:
+                    break;
+            }
+
+            switch (d)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    d = "0" + d;
+                    break;
+                default:
+                    break;
+            }
+
+            switch (H)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    H = "0" + H;
+                    break;
+                default:
+                    break;
+            }
+
+            switch (m)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    m = "0" + m;
+                    break;
+                default:
+                    break;
+            }
+
+            return y + M + d + H + m;
+        }
+    }
+}
--- RUL/Magicpush/GetNotice.cs
+++ RUL/Magicpush/GetNotice.cs
@@ -1,7 +1,4 @@
-﻿using StriveEngine;
-using StriveEngine.Core;
-using StriveEngine.Tcp.Passive;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -79,61 +76,6 @@ namespace RUL.Magicpush
         httpWebResponse.Close();
         streamReader.Close();
         return responseContent;
-    }
-
-
-    // Todo
-    /*
-    private ITcpPassiveEngine tcpPassiveEngine;
-
-    public static Get()
-    {
-
-    }
-
-    void Init()
-    {
-        try
-        {
-            tcpPassiveEngine = NetworkEngineFactory.CreateTextTcpPassiveEngine("magicpush.romonov.com", 45679, new DefaultTextContractHelper("\0"));
-            tcpPassiveEngine.MessageReceived += new CbDelegate<IPEndPoint, byte[]>(tcpPassiveEngine_MessageReceived);
-            tcpPassiveEngine.AutoReconnect = false;               
-            tcpPassiveEngine.ConnectionInterrupted += new CbDelegate(tcpPassiveEngine_ConnectionInterrupted);
-            tcpPassiveEngine.ConnectionRebuildSucceed += new CbDelegate(tcpPassiveEngine_ConnectionRebuildSucceed);
-            tcpPassiveEngine.Initialize();
-
-            Logger.Info("Successful!");
-        }
-        catch (Exception e)
-        {
-            Logger.Error(e.Message);
-        }
-
-        tcpPassiveEngine.CloseConnection();
-    }
-
-    void tcpPassiveEngine_ConnectionRebuildSucceed()
-    {
-        Logger.Info("Reconnect successfully!");
-    }
-
-    void tcpPassiveEngine_ConnectionInterrupted()
-    {
-        Logger.Info("Disconnected!");
-    }
-
-    void tcpPassiveEngine_MessageReceived(IPEndPoint serverIPE, byte[] bMsg)
-    {
-        string msg = Encoding.UTF8.GetString(bMsg); //消息使用UTF-8编码
-        msg = msg.Substring(0, msg.Length - 1); //将结束标记"\0"剔除
-        Get(msg);
-    }
-
-    private void Send(object sender, EventArgs e)
-    {
-        string msg = this.textBox_msg.Text + "\0";// "\0" 表示一个消息的结尾
-        byte[] bMsg = Encoding.UTF8.GetBytes(msg);//消息使用UTF-8编码
-        this.tcpPassiveEngine.SendMessageToServer(bMsg);
     }
     */
     }
