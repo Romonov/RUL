3cbd1183b3fd2eefe32fd74f0d640d8a26e084ab
Romonov <Romonov2003@outlook.com>
2018-11-18 14:58:03 +0800

    增加发送附件功能

--- RUL/Net/EmailHelper.cs
+++ RUL/Net/EmailHelper.cs
@@ -2,12 +2,14 @@
 using System.Net.Mail;
 using System.Text;
 using System.Net;
+using System.Net.Mime;
+using System.IO;
 
 namespace RUL.Net
 {
     class EmailHelper
     {
-        public static bool MailSend(EmailParam EmailParams)
+        public static bool Send(EmailParam EmailParams)
         {
             try
             {
@@ -22,15 +24,31 @@ namespace RUL.Net
 
                         mailMessage.Body = EmailParams.SendContent;
                         mailMessage.BodyEncoding = Encoding.UTF8;
-                        mailMessage.IsBodyHtml = false;
+                        mailMessage.IsBodyHtml = EmailParams.IsSendContentHtml;
 
                         sendSmtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
-                        sendSmtpClient.EnableSsl = false;
+                        sendSmtpClient.EnableSsl = EmailParams.IsEnableSsl;
 
                         sendSmtpClient.UseDefaultCredentials = false;
 
                         NetworkCredential myCredential = new NetworkCredential(EmailParams.SendEmail, EmailParams.SendPwd);
                         sendSmtpClient.Credentials = myCredential;
+
+                        if (EmailParams.AttachmentPaths.Length > 0)
+                        {
+                            Attachment data;
+                            ContentDisposition disposition;
+                            for (int i = 0; i < EmailParams.AttachmentPaths.Length; i++)
+                            {
+                                data = new Attachment(EmailParams.AttachmentPaths[i], MediaTypeNames.Application.Octet);
+                                disposition = data.ContentDisposition;
+                                disposition.CreationDate = File.GetCreationTime(EmailParams.AttachmentPaths[i]);
+                                disposition.ModificationDate = File.GetLastWriteTime(EmailParams.AttachmentPaths[i]);
+                                disposition.ReadDate = File.GetLastAccessTime(EmailParams.AttachmentPaths[i]);
+                                mailMessage.Attachments.Add(data);
+                            }
+                        }
+
                         sendSmtpClient.Send(mailMessage);
                     }
                 }
@@ -49,6 +67,11 @@ namespace RUL.Net
         /// 邮件服务器
         /// </summary>
         public string SendSmtp { get; set; }
+
+        /// <summary>
+        /// 是否启用Ssl
+        /// </summary>
+        public bool IsEnableSsl { get; set; }
 
         /// <summary>
         /// 发件人邮箱
@@ -81,6 +104,11 @@ namespace RUL.Net
         public bool IsSendContentHtml { get; set; }
 
         /// <summary>
+        /// 发件附件
+        /// </summary>
+        public string[] AttachmentPaths { get; set; }
+
+        /// <summary>
         /// 收件人邮件地址 
         /// </summary>
         public string ConsigneeAddress { get; set; }
@@ -89,9 +117,6 @@ namespace RUL.Net
         /// 收件人名称
         /// </summary>
         public string ConsigneeName { get; set; }
-
-
-
 
     }
 }
